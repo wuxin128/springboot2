147c8c28be798d223037b887137ed40808490590
carl <carl@qq.com>
2022-04-07 16:08:41 +0800

    third commit

--- springboot-test/src/main/java/com/wang/springboot2/dao/User.java
+++ springboot-test/src/main/java/com/wang/springboot2/dao/User.java
@@ -7,5 +7,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("你好，git!");
         System.out.println("你好，git222!");
+        System.out.println("你好，git333!");
     }
 }
