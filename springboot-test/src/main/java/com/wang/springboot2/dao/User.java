17bd5800fe57022d277162dd5f66ce752f3bfb34
carl <carl@qq.com>
2022-04-07 16:05:37 +0800

    first commit

---
+++ springboot-test/src/main/java/com/wang/springboot2/dao/User.java
@@ -0,0 +1,10 @@
+package com.wang.springboot2.dao;/*
+@author carl
+@date 2022/4/7 - 16:00
+*/
+
+public class User {
+    public static void main(String[] args) {
+        System.out.println("你好，git!");
+    }
+}
